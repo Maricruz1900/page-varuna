4eecade933d352cb71ab8d7d5c865702398784ce
Maricruz1900 <utm22030695@utma.edu.mx>
2024-07-29 08:57:35 -0600

    feat: initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import Register from './Register';
+import Login from './Login';
+//import Contacto from './Contacto';
+//import MainPage from './components/MainPage';
+//import Formulario from './Formulario';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
